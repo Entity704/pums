e6be71fa1a2934e782ae6a82e02a5a9ce4a0ca6a
Entity704 <94740599+Entity704@users.noreply.github.com>
2024-08-30 16:57:18 +0800

    main

--- sim.js
+++ sim.js
@@ -5,9 +5,10 @@ var a;
 var money;
 var clicks = 0;
 var hits = 0;
+
 ot = Date.now();
 
-function randomMoney(isup) {
+function randomMoney() {
     hits += 1;
     document.getElementById('hits').innerHTML='命中次数: '+hits;
     a = Math.round(Math.random() * 10);
@@ -15,9 +16,7 @@ function randomMoney(isup) {
     money += 5;
     total += money + a;
 
-    if(isup) {
-        update();
-    }
+    update();
 }
 
 function update() {
@@ -25,32 +24,79 @@ function update() {
     document.getElementById('money').innerHTML='你捡到了: '+(money + a)+'个钱袋';
     document.getElementById('golds').innerHTML=parseInt(golds)+'个黄金 '+silver+'个白银';
 
-    var history = document.createElement('p');
-    history.innerText = '*** 你捡到了'+money+'+'+a+'个钱袋';
-    history.style.lineHeight = '80%';
-    history.style.fontSize = '8px';
-
-    var addressContainer = document.getElementById('history');
-    addressContainer.insertAdjacentElement('afterbegin', history);
+    show('*** 你 捡到钱袋了! 拿到了 '+a+' + '+money+' 个钱袋!');
+    show('*** 你获得了 钱袋x'+(money+a));
 
     ot = Date.now();
 }
 
+function show(smsg) {
+    var msg = document.createElement('p');
+    msg.innerHTML= smsg;
+    var addr = document.getElementById('history');
+    addr.insertAdjacentElement('afterbegin', msg);
+}
+
 function usingBag() {
-    golds += total * (1 - Math.random() / 10);
-    golds = parseFloat(golds.toFixed(5));
-    silver = Math.round((golds - parseInt(golds)) * 1e5);
-    total = 0;
-    document.getElementById('total').innerHTML='总计: '+total+'个钱袋';
+    hgold = parseFloat((total * (1.1 - Math.random() / 5)).toFixed(4))
+    golds += hgold;
+    golds = parseFloat(golds.toFixed(4));
+    silver = Math.round((golds - parseInt(golds)) * 1e4);
     document.getElementById('golds').innerHTML=parseInt(golds)+'个黄金 '+silver+'个白银';
 
-    clicks -= 1;
-    document.getElementById('clicks').innerHTML='点击次数: '+clicks;
+    if(total != 0) {
+        show('*** 你 使用了物品:钱袋 x'+(money + a)+' ,获得了 '+parseInt(hgold)+' 黄金与 '+Math.round((hgold - parseInt(hgold)) * 1e4)+' 白银');
+    }
+
+    total = 0;
+    document.getElementById('total').innerHTML='总计: '+total+'个钱袋';
+}
+
+function detkey(e) {
+    e = e||window.event;
+    if(e.keyCode == 13) {
+        var msg = document.createElement('p');
+        msg.innerHTML='0: 你: '+document.getElementById('say').value;
+        document.getElementById('say').value='';
+        msg.style.color = '#ffffff';
+        var addr = document.getElementById('history');
+        addr.insertAdjacentElement('afterbegin', msg);
+    }
+}
+
+function eval(evaluation, color) {
+    var seval = document.getElementById('seval');
+    seval.style.color = color;
+    seval.innerHTML = evaluation;
 }
 
 window.onload = function() {
-    document.onmousedown = function(event) {
+    var mb = document.getElementById('moneybtn');
+    var hb = document.getElementById('hitbox');
+    var rh = document.getElementById('rhit');
+
+    rh.onclick = function(e) {
+        dis = mb.getBoundingClientRect().left - hb.getBoundingClientRect().left;
         clicks += 1;
         document.getElementById('clicks').innerHTML='点击次数: '+clicks;
+        if(dis >= -30 & dis <= 30) {
+            randomMoney();
+        }
+
+        if(dis >= -5 & dis <= 5) {
+            eval('完美', 'forestgreen');
+        }
+        else if(dis >= -15 & dis <= 15) {
+            eval('优秀', 'limegreen');
+        }
+        else if(dis >= -24 & dis <= 24) {
+            eval('还行', '#b9f72b');
+        }
+        else if(dis >= -30 & dis <= 30) {
+            eval('一般', 'gold');
+        }
+        else {
+            eval('错过', 'red');
+        }
     }
 }
